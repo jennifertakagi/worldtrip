a190295ab8aac815a97d0dcf1e917a07be0649c9
jennifertakagi <jennitakagi@gmail.com>
2021-08-22 18:03:29 -0300

    chore: add service and set html structure

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,13 @@
-import { AppProps } from 'next/app';
-import { ChakraProvider } from '@chakra-ui/react';
-import { theme } from '../styles/theme';
+import { AppProps } from "next/app";
+import { ChakraProvider } from "@chakra-ui/react";
+
+import { theme } from "../styles/theme";
+import "../styles/slide.scss"
+
+import "swiper/swiper.scss";
+import "swiper/components/navigation/navigation.scss";
+import "swiper/components/pagination/pagination.scss";
+import "swiper/components/scrollbar/scrollbar.scss";
 
 function MyApp({ Component, pageProps }: AppProps): JSX.Element {
   return (
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -7,6 +7,7 @@ export default class MyDocument extends Document {
         <Head>
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700&display=swap" rel="stylesheet"/>
+          <link rel="stylesheet" href="https://unpkg.com/swiper/swiper-bundle.min.css" />
         </Head>
         <body>
           <Main />
